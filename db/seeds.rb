3dadce479c45abf80e49d5539fd21a4db736828b
romain nicod <ai-gmented@pm.me>
2026-08-14 11:43:26 +0200

    replace

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "destroying movies, list and bookmark"
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "creating 4 movies"
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 69)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 87)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 79)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 70)
+
+puts "creating 2 lists"
+List.create(name: "Horror")
+List.create(name: "Funny")
+
+puts "creating 6 bookmarks"
+
+6.times do
+  begin
+    list = List.all.sample
+    movie = Movie.all.sample
+    Bookmark.create!(comment: "bon film", movie: movie, list: list)
+  rescue
+    puts "Bookmark already existing"
+  end
+end
+
+puts "created #{Bookmark.count} bookmarks"
